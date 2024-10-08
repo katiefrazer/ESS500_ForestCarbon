fa8dbd8d0cc7faf4d93c34823539a8d5ed17b090
Katie Frazer <katherinemarijke@gmail.com>
2024-10-08 16:19:26 -0600

    setup script and ground cover aspen meadows work

---
+++ scripts/setup.R
@@ -0,0 +1,17 @@
+# a script for installing (if needed) and loading packages for this project
+
+packageLoad <- function(x) {
+  for (i in 1:length(x)) {
+    if (!x[i] %in% installed.packages()) {
+      install.packages(x[i])
+    }
+    library(x[i], character.only = TRUE)
+  }
+}
+
+packages <- c("tidyverse",
+              "readxl",
+              "rmarkdown",
+              "lubridate")
+
+packageLoad(packages)
